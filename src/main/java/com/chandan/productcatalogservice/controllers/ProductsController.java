c5f9e9a98312c45e78512518e2d45792929ecbc8
chandan kumar <ckumar2@deloitte.com>
2024-04-21 21:58:53 +0530

    create products method added in controller

--- src/main/java/com/chandan/productcatalogservice/controllers/ProductsController.java
+++ src/main/java/com/chandan/productcatalogservice/controllers/ProductsController.java
@@ -1,11 +1,9 @@
 package com.chandan.productcatalogservice.controllers;
 
 
+import com.chandan.productcatalogservice.dtos.ProductDto;
 import com.chandan.productcatalogservice.models.Product;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
@@ -15,6 +13,14 @@ public class ProductsController {
     @GetMapping("{id}")
     public Product getProductById(@PathVariable("id") Long id ){
         Product product = new Product();
+
         return product;
+    }
+
+    @PostMapping
+    public ProductDto createProduct(@RequestBody ProductDto productDto){
+
+        ProductDto productDto1 = new ProductDto();
+        return productDto1;
     }
 }
